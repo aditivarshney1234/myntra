6168a0c78d071c291cb30900bd7957e6135cb5bd
aditivarshney1234 <aditivarshney942@gmail.com>
2024-07-15 23:24:58 +0530

    Add files via upload

---
+++ myntra_clone-master/myntra_clone-master/js/temp.js
@@ -0,0 +1,46 @@
+products = [
+    {
+        brand_name: "FIDO DIDO",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/2476463/2018/2/10/11518244710081-FIDO-DIDO-Men-Mustard-Printed-Polo-Collar-T-shirt-5511518244709858-1.jpg",
+        T_shirt_name: "Solid Polo Collar T-shirt",
+        price: 629,
+        discount: 10,
+        oldprice: 699,
+    },
+    {
+        brand_name: "Levis",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/13298386/2021/3/3/a45e9273-cb98-407b-8bf3-7c1287716e2b1614762063196-Levis-Men-Red--Navy-Blue-Colourblocked-Round-Neck-T-shirt-wi-1.jpg",
+        T_shirt_name: "coloured Round neck T-shirt",
+        price: 1199,
+        discount: 20,
+        oldprice: 1499,
+
+    },
+    {
+        brand_name: "Levis",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/13298380/2021/1/27/0dd95d61-be30-4eea-9fc1-0373cb7cc38a1611751095466-Levis-Men-Tshirts-4771611751093290-1.jpg",
+        T_shirt_name: "Printed round neck T-shirt",
+        price: 800,
+        discount: 20,
+        oldprice: 999,
+
+    },
+    {
+        brand_name: "WROGN",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/1894413/2017/6/20/11497943671766-WROGN-Men-Grey-Melange-Self-Design-Round-Neck-T-shirt-881497943671356-1.jpg",
+        T_shirt_name: "Self Design round neck T-shirt",
+        price: 792,
+        discount: 10,
+        oldprice: 879,
+
+    },
+    {
+        brand_name: "WROGN",
+        img: "https://assets.myntassets.com/f_webp,dpr_1.5,q_60,w_210,c_limit,fl_progressive/assets/images/10188253/2019/9/26/1c589968-d8a0-43e6-acc6-8a22494c85a81569502754494-WROGN-Men-Off-White-Printed-Hood-T-shirt-1061569502752645-1.jpg",
+        T_shirt_name: "Printed Hood T-shirt",
+        price: 629,
+        discount: 10,
+        oldprice: 699,
+
+    },
+]
